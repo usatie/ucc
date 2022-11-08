19352bca690fb8bf92b129ba9d3ffdcaa458c834
Shun Usami <usatie@gmail.com>
2022-11-08 16:21:19 +0900

    :sparkles: Add '+' and '-' operators

--- ucc.c
+++ ucc.c
@@ -2,16 +2,33 @@
 #include <stdlib.h>
 
 int	main(int argc, char *argv[]) {
-	(void)argv;
 	if (argc != 2) {
 		fprintf(stderr, "Invalid number of args\n");
 		return 1;
 	}
 
+	char *p = argv[1];
+
 	printf(".intel_syntax noprefix\n");
 	printf(".globl main\n");
 	printf("main:\n");
-	printf("  mov rax, %d\n", atoi(argv[1]));
+	printf("  mov rax, %ld\n", strtol(p, &p, 10));
+
+	while (*p) {
+		if (*p == '+') {
+			p++;
+			printf("  add rax, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+		if (*p == '-') {
+			p++;
+			printf("  sub rax, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+
+		fprintf(stderr, "unexpected character: '%c'\n", *p);
+		return 1;
+	}
 	printf("  ret\n");
 	return 0;
 }
